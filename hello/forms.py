1365fc328e72c2a8180355b5f0c42bf092f26301
mori_yae <mori_yae@worksap.co.jp>
2023-02-21 23:28:53 +0900

    form

--- hello/forms.py
+++ hello/forms.py
@@ -1,12 +1,21 @@
-from django import forms
+from django import forms #☆
+from.models import Friend
 
-class HelloForm(forms.Form):
-    data=[
-        ('one','item 1'),
-        ('two','item 2'),
-        ('three','item 3'),
-        ('four','item 4'),
-        ('five','item 5'),        
-    ]
-    choice = forms.ChoiceField(label='item', \
-        choices=data,widget=forms.SelectMultiple(attrs={'size': 6}))
+class FriendForm(forms.ModelForm):
+    class Meta:
+        model = Friend
+        fields = ['name','mail','gender','age','birthday']
+
+class FindForm(forms.Form):
+    find = forms.CharField(label='Find', required=False,\
+            widget=forms.TextInput(attrs={'class':'form-control'}))
+
+class CheckForm(forms.Form):
+    str = forms.CharField(label='String', \
+        widget=forms.TextInput(attrs={'class':'form-control'}))
+
+    def clean(self):
+        cleaned_date = super().clean()
+        str = cleaned_date['str']
+        if(str.lower().startswith('no')):
+            raise forms.ValidationError('You input "NO"!')
